3c3b3edb1a31ad12095e9f5274073a467a516b5d
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-24 01:01:03 -0600

    some updates into the AppContext

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,11 +1,21 @@
 /** @format */
 
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-  const value = {};
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  const [userData, setUserData] = useState(false);
+
+  const value = {
+    backendUrl,
+    isLoggedin,
+    setIsLoggedin,
+    userData,
+    setUserData,
+  };
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
