f7fa996e193e794495abe8109f4967106dcab46a
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-23 22:41:58 -0600

    update header navbar and link the login button to login page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div>
+    <div className=" flex flex-col items-center justify-center min-h-screen ">
       <Navbar />
       <Header />
     </div>
