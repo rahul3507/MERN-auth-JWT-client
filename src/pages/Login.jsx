f2be5214e21f9a831fa5066ba8d8cd92f217ce70
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-23 23:24:57 -0600

    updated login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,48 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
 
 const Login = () => {
+  const [state, setState] = useState("Sign Up");
   return (
-    <div>
-      <h1>login</h1>
+    <div
+      className="flex items-center justify-center min-h-screen px-6 
+    sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400 "
+    >
+      <img
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer "
+        src={assets.logo}
+        alt=""
+      />
+      <div
+        className=" bg-slate-900 p-10 rounded-lg shadow-lg w-full
+      sm:w-96 text-indigo-300 text-sm"
+      >
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === "Sign Up" ? "Create account" : "Login"}
+        </h2>
+        <p className="text-sm text-center mb-3">
+          {state === "Sign Up"
+            ? "Create your account"
+            : "Login to your account"}
+        </p>
+
+        <form>
+          <div
+            className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full
+           bg-blue-900"
+          >
+            <img src={assets.person_icon} alt="" />
+            <input
+              className="bg-transparent outline-none h-full text-white"
+              type="text"
+              placeholder="Full Name"
+              required
+            ></input>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
