8862dff0536038aef5ee04f47eec15423695c20d
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-24 00:46:25 -0600

    created the appcontext

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { createContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+  const value = {};
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
